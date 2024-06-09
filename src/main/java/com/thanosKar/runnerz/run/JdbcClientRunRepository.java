656c98a05a3c106d89efedbf9e90ab482994e994
ThanosKarWeb <thanos.kar.web@gmail.com>
2024-06-09 12:34:32 +0300

    removed commented out stuff

--- src/main/java/com/thanosKar/runnerz/run/JdbcClientRunRepository.java
+++ src/main/java/com/thanosKar/runnerz/run/JdbcClientRunRepository.java
@@ -8,48 +8,21 @@ import org.springframework.util.Assert;
 import java.util.List;
 import java.util.Optional;
 
-//Repository Annotation
-/*
- *Indicates that an annotated class is a "Repository", originally defined by Domain-Driven Design (Evans, 2003) as
- *"a mechanism for encapsulating storage, retrieval, and search behavior which emulates a collection of objects".
- *Teams implementing traditional Jakarta EE patterns such as "Data Access Object" may also apply this stereotype to DAO classes,
- *though care should be taken to understand the distinction between Data Access Object and DDD-style repositories before doing so.
- *This annotation is a general-purpose stereotype and individual teams may narrow their semantics and use as appropriate.
- *A class thus annotated is eligible for Spring DataAccessException translation when used in conjunction with a
- *PersistenceExceptionTranslationPostProcessor. The annotated class is also clarified as to its role in the overall application
- *architecture for the purpose of tooling, aspects, etc.
- *
- *This annotation also serves as a specialization of @Component, allowing for implementation classes to be auto-detected through
- *classpath scanning.
- */
+
 @Repository
 public class JdbcClientRunRepository {
-
-    //JdbcClient
-/*
-A fluent JdbcClient with common JDBC query and update operations, supporting JDBC-style positional
-as well as Spring-style named parameters with a convenient unified facade for JDBC PreparedStatement
-execution.
-    An example for retrieving a query result as a java.util.Optional:
-    Optional<Integer> value = client.sql("SELECT AGE FROM CUSTOMER WHERE ID = :id")
-    .param("id", 3)
-    .query(Integer.class)
-    .optional();
-    */
     private final JdbcClient jdbcClient;
 
     public JdbcClientRunRepository(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;//dependency injection
+        this.jdbcClient = jdbcClient;
     }
 
-    //Return all records
     public List<Run> findAll(){
         return jdbcClient.sql("select * from run")
                 .query(Run.class)
                 .list();
     }
 
-    //Return a single record by its id
     public Optional<Run> findById(Integer id){
         return jdbcClient.sql("SELECT id, title,started_on,completed_on,miles,location FROM Run WHERE id = :id")
                 .param("id", id)
@@ -57,16 +30,14 @@ execution.
                 .optional();
     }
 
-    //Create a new record
     public void create(Run run){
         var updated = jdbcClient.sql("INSERT INTO Run(id, title, started_on, completed_on, miles, location) VALUES(?,?,?,?,?,?)")
                 .params(List.of(run.id(),run.title(),run.startedOn(),run.completedOn(),run.miles(),run.location().toString()))
-                .update();//inserting/updating/deleting something
+                .update();
 
         Assert.state(updated == 1, "Failed to create run " + run.title());
     }
 
-    //Update a record given the id
     public void update(Run run, Integer id){
         var updated = jdbcClient.sql("UPDATE Run SET title = ?, started_on = ?, completed_on = ?, miles = ?, location = ? WHERE id = ?")
                 .params(List.of(run.title(),run.startedOn(),run.completedOn(),run.miles(),run.location().toString(),id))
@@ -75,7 +46,6 @@ execution.
         Assert.state(updated == 1, "Failed to update run " + run.title());
     }
 
-    //Delete a record given the id
     public void delete(Integer id){
         var updated = jdbcClient.sql("DELETE FROM Run WHERE id = :id")
                 .param("id",id)
@@ -84,17 +54,14 @@ execution.
         Assert.state(updated == 1, "Failed to delete run " + id);
     }
 
-    //Return the amount of records
     public int count() {
         return jdbcClient.sql("SELECT * FROM Run").query().listOfRows().size();
     }
 
-    //Save all records
     public void saveAll(List<Run> runs){
         runs.stream().forEach(this::create);
     }
 
-    //Return all records given the location
     public List<Run> findByLocation(String location){
         return jdbcClient.sql("SELECT * FROM Run WHERE location = :location")
                 .param("location",location)
